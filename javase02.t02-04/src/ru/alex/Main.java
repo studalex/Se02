4470973ca370ace5effe0f49f012fc991239106d
studalex <stud.alex.2302@gmail.com>
2015-10-05 04:27:45 +0300

    Se02

--- javase02.t02-04/src/ru/alex/Main.java
+++ javase02.t02-04/src/ru/alex/Main.java
@@ -6,6 +6,7 @@ import ru.alex.stationery.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,6 +21,7 @@ public class Main {
         Pen pen = new Pen(30,"Черная ручка", Color.BLACK, Pen.Type.INK);
         Pencil pencil = new Pencil(21, "Карандаш", Color.BLACK, Pencil.Type.GRAPH);
         Pencil pencil2 = new Pencil(20, "Карандаш", Color.BLACK, Pencil.Type.GRAPH);
+        Pencil pencil3 = new Pencil(20, "Арандаш", Color.BLACK, Pencil.Type.GRAPH);
         Notebook notebook = new Notebook(2, "Тетрадь", PaperSupplies.Format.A5, 96);
 
         List beginnerSet = new ArrayList();
@@ -27,6 +29,7 @@ public class Main {
         beginnerSet.add(pencil);
         beginnerSet.add(notebook);
         beginnerSet.add(pencil2);
+        beginnerSet.add(pencil3);
 
         //Создаем нового сотрудника со статусом новичек
         Employees employees = new Employees("Александр Пушкин", Employees.Status.BEGINNER);
@@ -36,6 +39,7 @@ public class Main {
         System.out.println("Полная  стоимость канцтоваров: " + employees.costOfAllStationery());
         System.out.println();
 
+
         System.out.println("Список канцтоваров без сортировки:");
         for(Object obj: beginnerSet)
         {
@@ -43,7 +47,8 @@ public class Main {
         }
         System.out.println();
 
-        System.out.println("Сортировка по цене:");
+        System.out.println("Сортировка по цене и имени:");
+        Collections.sort(beginnerSet);
         Collections.sort(beginnerSet, new Pen());
         for(Object obj: beginnerSet)
         {
@@ -53,6 +58,14 @@ public class Main {
 
         System.out.println("Сортировка по имени:");
         Collections.sort(beginnerSet);
+        for(Object obj: beginnerSet)
+        {
+            System.out.println((Stationery)obj);
+        }
+        System.out.println();
+
+        System.out.println("Сортировка по цене:");
+        Collections.sort(beginnerSet, new Pen());
         for(Object obj: beginnerSet)
         {
             System.out.println((Stationery)obj);
